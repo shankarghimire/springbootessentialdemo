3d4939bf164e3cd0ecad5209e8c9f8b1e0a9fb18
Shankar Ghimire <ssghimire@gmail.com>
2020-10-01 21:35:41 -0400

    Initializes Spring Boot Project

---
+++ src/main/java/ca/sheridancollege/ghimirsh/springbootessentialdemo/SpringbootessentialdemoApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.ghimirsh.springbootessentialdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootessentialdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootessentialdemoApplication.class, args);
+    }
+
+}
