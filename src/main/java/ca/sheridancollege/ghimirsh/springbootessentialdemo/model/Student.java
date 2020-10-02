67a802fb2bae26cf9a262d7ce6cf7c5aaf9157ec
Shankar Ghimire <ssghimire@gmail.com>
2020-10-01 22:43:20 -0400

    Adds Student.java model to the project

---
+++ src/main/java/ca/sheridancollege/ghimirsh/springbootessentialdemo/model/Student.java
@@ -0,0 +1,50 @@
+package ca.sheridancollege.ghimirsh.springbootessentialdemo.model;
+
+import java.util.UUID;
+
+public class Student {
+    private final UUID id;
+    private final Integer age;
+    private  final String firstName;
+    private  final  String lastName;
+    private  final  String  course;
+
+    public Student(UUID id, Integer age, String firstName, String lastName, String course) {
+        this.id = id;
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.course = course;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", age=" + age +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", course='" + course + '\'' +
+                '}';
+    }
+}
